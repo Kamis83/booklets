c48c545310fbcf83d53122e749d6ce48b47f0781
kamil.nagorski <bajerka@gmail.com>
2022-09-15 13:55:45 +0200

    ComplexBookletFactory: - lookForGrammagesInRawMaterials further  work

--- src/main/java/pl/kamis83/booklet/service/ComplexBookletFactory.java
+++ src/main/java/pl/kamis83/booklet/service/ComplexBookletFactory.java
@@ -3,13 +3,15 @@ package pl.kamis83.booklet.service;
 import org.springframework.stereotype.Component;
 import pl.kamis83.booklet.domain.ComplexBooklet;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @Component
 public class ComplexBookletFactory {
 
-    static final Pattern grammagePattern = Pattern.compile("[\\d+]+");
+    static final Pattern grammagePattern = Pattern.compile("[\\d+]+[^\\sg/m2,]");
     int pageMiddleUnitCount = 40;
     int pageCoverCount = 4;
     double xDimension = 297;
@@ -17,29 +19,39 @@ public class ComplexBookletFactory {
     String certificate = "FSC MIX CREDIT";
 
 
-
-
-    public ComplexBooklet complexBookletFactory(String name, String format, String rawMaterials) {
+    public ComplexBooklet createComplexBookletFactory(String name, String format, String rawMaterials) {
 
         Matcher matcher = ComplexBookletFactory.grammagePattern.matcher(rawMaterials);
-        int unitGrammage = getGrammages(matcher);
-        int coverGrammage = getGrammages(matcher);
-        if (unitGrammage > coverGrammage) {
-            int temporaryCoverGrammage = unitGrammage;
-            unitGrammage = coverGrammage;
-            coverGrammage = temporaryCoverGrammage;
-        }
+        List<Integer> grammages = lookForGrammagesInRawMaterials(matcher);
+        int coverGrammage = grammages.get(0);
+        int unitGrammage = grammages.get(1);
 
-        return new ComplexBooklet (pageMiddleUnitCount, pageCoverCount, xDimension, yDimension,
+
+        return new ComplexBooklet(pageMiddleUnitCount, pageCoverCount, xDimension, yDimension,
                 unitGrammage, coverGrammage, certificate);
     }
 
-    public  int getGrammages(Matcher matcher) {
+    public List<Integer> lookForGrammagesInRawMaterials(Matcher matcher) {
+        return getIntegers(getSingleGrammageFromRawMaterials(matcher), getSingleGrammageFromRawMaterials(matcher));
+    }
+
+    public int getSingleGrammageFromRawMaterials(Matcher matcher) {
         int grammage = 0;
         if (matcher.find()) {
             grammage = Integer.parseInt(matcher.group(0));
+        }
+        return grammage;
+    }
 
-
-        }return grammage;
+    private static List<Integer> getIntegers(int firstGrammage, int secondGrammage) {
+        List<Integer> grammages = new ArrayList<>();
+        if (firstGrammage == 0 || secondGrammage != 0 && firstGrammage > secondGrammage) {
+            grammages.add(0, firstGrammage);
+            grammages.add(1, secondGrammage);
+        } else if ((secondGrammage == 0 || firstGrammage < secondGrammage)) {
+            grammages.add(0, secondGrammage);
+            grammages.add(1, firstGrammage);
+        }
+        return grammages;
     }
 }
--- src/test/java/pl/kamis83/booklet/service/ComplexBookletFactoryTest.java
+++ src/test/java/pl/kamis83/booklet/service/ComplexBookletFactoryTest.java
@@ -2,6 +2,7 @@ package pl.kamis83.booklet.service;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.regex.Matcher;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,31 +10,55 @@ import static org.junit.jupiter.api.Assertions.*;
 class ComplexBookletFactoryTest {
 
     @Test
-    void getGrammage() {
+    void getOneGrammage() {
         //given
       ComplexBookletFactory complexBookletFactory = new ComplexBookletFactory();
         String rawMaterials = "kreda mat 80g/m";
         Matcher matcher = ComplexBookletFactory.grammagePattern.matcher(rawMaterials);
         //when
-        int result = complexBookletFactory.getGrammages(matcher);
+        int result = complexBookletFactory.getSingleGrammageFromRawMaterials(matcher);
         //Then
 
         assertEquals(80,result);
 
     }
     @Test
-    void getGrammages() {
+    void getTwoGrammages() {
         //given
         ComplexBookletFactory complexBookletFactory = new ComplexBookletFactory();
-        String rawMaterials = "kreda mat 80g/m, kreda 90 g/m";
+        String rawMaterials = "Kreda mat 170 g/m2, Kreda mat 90 g/m2";
         Matcher matcher = ComplexBookletFactory.grammagePattern.matcher(rawMaterials);
         //when
-        int firstGrammage = complexBookletFactory.getGrammages(matcher);
-        int secondGrammage = complexBookletFactory.getGrammages(matcher);
+        int firstGrammage = complexBookletFactory.getSingleGrammageFromRawMaterials(matcher);
+        int secondGrammage = complexBookletFactory.getSingleGrammageFromRawMaterials(matcher);
         //Then
 
-        assertEquals(80,firstGrammage);
+        assertEquals(170,firstGrammage);
         assertEquals(90,secondGrammage);
+
+    }
+
+    @Test
+    void findGrammage() {
+
+        //given
+        ComplexBookletFactory complexBookletFactory = new ComplexBookletFactory();
+        String rawMaterials = "Kreda mat g/m2, Kreda mat 80 g/m2";
+        Matcher matcher = ComplexBookletFactory.grammagePattern.matcher(rawMaterials);
+
+        //when
+        List<Integer> grammages = complexBookletFactory.lookForGrammagesInRawMaterials(matcher);
+        int coverGrammage = grammages.get(0);
+        int unitGrammage = grammages.get(1);
+
+
+        //then
+        assertEquals(0,coverGrammage);
+        assertEquals(80,unitGrammage);
+
+
+
+
 
     }
 }
